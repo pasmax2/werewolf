f9845419bb9e368df79818f19fbe07b6cdbbeaea
GewoonYorick <github@gewoonyorick.nl>
2018-06-28 19:55:04 +0200

    Changed Min/Max players

--- settings/sabotage.py
+++ settings/sabotage.py
@@ -2,5 +2,5 @@ MINIMUM_WAIT = 60 # seconds before game can be started
 EXTRA_WAIT = 20
 MAXIMUM_WAITED = 2  # number of times !wait can be used
 
-MAX_PLAYERS = 5
-MIN_PLAYERS = 5
+MAX_PLAYERS = 15
+MIN_PLAYERS = 2
